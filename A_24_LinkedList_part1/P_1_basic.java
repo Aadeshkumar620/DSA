3c3e6a0cc67d1b82779e2cfa91de2d7fce697c2e
Aadesh Kumar <162555223+Aadeshkumar620@users.noreply.github.com>
2025-03-11 18:01:58 +0000

    Commit

---
+++ A_24_LinkedList_part1/P_1_basic.java
@@ -0,0 +1,22 @@
+package A_24_LinkedList_part1;
+
+public class P_1_basic {
+    public static class Node{
+        int data;     //Value
+        Node next;   //address of next node
+
+        Node(int data){
+            this.data = data;
+        }
+    }
+    
+    public static void main(String[] args) {
+        Node a = new Node(1);
+        Node b = new Node(2);
+        Node c = new Node(3);
+        Node d = new Node(4);
+        Node e = new Node(5);
+       
+    }
+    
+}
