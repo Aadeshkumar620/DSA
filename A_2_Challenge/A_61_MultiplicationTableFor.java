be94321f5a43bf5dd8baf6613437951fdfa423b9
Aadesh Kumar <162555223+Aadeshkumar620@users.noreply.github.com>
2025-03-17 12:29:02 +0000

    Commit

---
+++ A_2_Challenge/A_61_MultiplicationTableFor.java
@@ -0,0 +1,17 @@
+package A_2_Challenge;
+
+import java.util.Scanner;
+
+class A_61_MultiplicationTableFor {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Welcome to printing tables\n");
+        System.out.print("Please enter the number you want to print table of: ");
+        int num = input.nextInt();
+
+        for (int i = 1; i <= 10; i++) {
+            System.out.println(num + " X " + i + " = " + (num * i));
+        }
+    }
+}
+
---
+++ A_2_Challenge/A_64_OccurrencesForEach.java
@@ -0,0 +1,30 @@
+package A_2_Challenge;
+
+import java.util.Scanner;
+
+class A_64_OccurrencesForEach {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Welcome to finding occurrences");
+        int[] nums = ArrayUtility.inputArray();
+        System.out.print("Now, enter the number you want to search: ");
+        int element = input.nextInt();
+        int occ = countOccurrences(nums, element);
+        System.out.println("Your element was found: " + occ + " times.");
+    }
+
+    public static int countOccurrences(int[] nums, int element) {
+        int occ = 0;
+        for (int num : nums) {
+            if (num == element) {
+                occ++;
+            }
+        }
+        return occ;
+    }
+}
+
+
+
+
+
